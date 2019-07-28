47c6f8c785dc3389b130fe34622c52d7d78a1ccf
Amir <khanamir448@gmail.com>
2019-07-28 12:45:07 +0530

    updates

--- KAActivityLoader/KAActivityLoader/KAActivityLoaderView.swift
+++ KAActivityLoader/KAActivityLoader/KAActivityLoaderView.swift
@@ -45,7 +45,7 @@ class KAActivityLoaderView: UIView {
         let y = (self.bounds.size.height / 2) - 20
         
         activityIndicator.frame = CGRect(x: x, y: y, width: 40, height: 40)
-        activityIndicator.style = .whiteLarge
+        activityIndicator.activityIndicatorViewStyle = .whiteLarge
         activityIndicator.hidesWhenStopped = true
         
         self.addSubview(activityIndicator)
